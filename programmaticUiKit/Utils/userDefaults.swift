cd646c35a20f68346922042eb058c610439c7a12
Kevsl <ktr2@pm.me>
2023-10-02 10:41:08 +0200

    feat: added userdefaults utils

--- programmaticUiKit/Utils/userDefaults.swift
+++ programmaticUiKit/Utils/userDefaults.swift
@@ -2,6 +2,23 @@ import UIKit
 
 let defaults = UserDefaults.standard
 
+
+func setupUDString(_ value:String,_ key:String  ){
+    defaults.set(value, forKey: key)
+}
+
+func readUDString(_ key: String) -> String{
+    
+    guard let selectedString = defaults.string(forKey: key) else{
+        return "error"
+    }
+    
+return selectedString
+
+
+}
+
+
 //
 //defaults.set("voiture", forKey: "Vehicle")
 //
